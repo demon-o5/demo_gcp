e3db5887e370534626d91f3471d1eb3345bf62ad
demon-o5 <129477612+demon-o5@users.noreply.github.com>
2023-09-13 19:59:56 +0530

    Create bucket.tf

---
+++ bucket.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  project     = "peppy-transit-398509"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
